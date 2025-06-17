e5ae6f4ecec6cc5399e1c837b8971efc5fe9931a
AliceMugure <machariamugure17@gmail.com>
2025-06-17 19:20:35 +0300

    added controllers and blueprints to app.py

--- server/app.py
+++ server/app.py
@@ -17,3 +17,12 @@ def create_app():
     from server.models.pizza import Pizza
     from server.models.restaurant_pizza import RestaurantPizza
     
+    from server.controllers.restaurant_controller import restaurants_bp
+    from server.controllers.pizza_controller import pizzas_bp
+    from server.controllers.restaurant_pizza_controller import restaurant_pizzas_bp
+    
+    app.register_blueprint(restaurants_bp)
+    app.register_blueprint(pizzas_bp)
+    app.register_blueprint(restaurant_pizzas_bp)
+    
+    return app
