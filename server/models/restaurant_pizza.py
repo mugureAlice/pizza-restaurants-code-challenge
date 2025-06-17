e1f2d5bfe1f08446e5a22cef5b234cf98f04365f
AliceMugure <machariamugure17@gmail.com>
2025-06-17 16:42:50 +0300

    Input Restaurants Data In the Models folder

---
+++ server/models/restaurant_pizza.py
@@ -0,0 +1,27 @@
+from server import db
+from sqlalchemy import CheckConstraint
+
+class RestaurantPizza(db.Model):
+    __tablename__ = 'restaurant_pizzas'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    price = db.Column(db.Integer, nullable=False)
+    pizza_id = db.Column(db.Integer, db.ForeignKey('pizzas.id'), nullable=False)
+    restaurant_id = db.Column(db.Integer, db.ForeignKey('restaurants.id'), nullable=False)
+    
+    __table_args__ = (
+        CheckConstraint('price >= 1 AND price <= 30', name='check_price_range'),
+    )
+    
+    def __repr__(self):
+        return f'<RestaurantPizza ${self.price}>'
+    
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'price': self.price,
+            'pizza_id': self.pizza_id,
+            'restaurant_id': self.restaurant_id,
+            'pizza': self.pizza.to_dict(),
+            'restaurant': self.restaurant.to_dict()
+        }
